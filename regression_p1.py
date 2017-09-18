aa8e6486aba2cb455aad3eb335f3b16bf00d0565
Saurabh Rao <rao.saurabh94@gmail.com>
2017-09-18 19:41:09 -0400

    documentation
    added documentation ( incomplete ) for users to understand how the code
    works .
    Next step , convert this into a flowchart visual .

--- regression_p1.py
+++ regression_p1.py
@@ -1,6 +1,26 @@
+#!/usr/bin/env python
 
-import pandas as pd 
-import quandl
+# make sure to install these packages before running:
+# pip install pandas
+# pip install sodapy
 
-df=quandl.get('WIKI/GOOGL')
-print(df) 
+import pandas as pd
+from sodapy import Socrata
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+client = Socrata("data.smcgov.org", None)
+
+# Example authenticated client (needed for non-public datasets):
+# client = Socrata(data.smcgov.org,
+#                  MyAppToken,
+#                  userame="user@example.com",
+#                  password="AFakePassword")
+
+# First 2000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("hwna-mxa3", limit=2000)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
+print(head(results_df))
